294c7c6124ccdda48645f03bacc7611f50b77b47
Yuling <hyl2099@gmail.com>
2020-09-16 15:04:02 +0200

    change repository

--- src/main/java/com/example/onlineMenu/repository/OrderRepository.java
+++ src/main/java/com/example/onlineMenu/repository/OrderRepository.java
@@ -1,8 +1,13 @@
 package com.example.onlineMenu.repository;
 
 import com.example.onlineMenu.documents.Order;
+import com.example.onlineMenu.documents.OrderDishList;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<Order, Long> {
+import java.util.List;
 
+public interface OrderRepository extends CrudRepository<Order, Long> {
+    @Query(value = "SELECT o.id, o.userWeChat, o.description, o.order_price, o.actual_price, o.mobile, o.order_status, o.add_time, o.pay_time, FROM Order o, OrderDishList od WHERE o.id = od.orderId")
+    public List<OrderDishList> findOrderDish();
 }
